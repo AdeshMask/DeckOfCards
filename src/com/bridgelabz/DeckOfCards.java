06634bb11825491094831475ebc74ab31089d4ce
AdeshMask <adeshmaske255@gmail.com>
2022-04-04 20:39:36 +0530

    Added: Initiating Game

---
+++ src/com/bridgelabz/DeckOfCards.java
@@ -0,0 +1,44 @@
+package com.bridgelabz;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class DeckOfCards {
+
+    String[] suit = {"Clubs", "Diamond","Heart","Spades"};
+    String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King", "Ace"};
+    String[][] combination = new String[4][13];
+    String[][] player = new String[2][9];
+
+
+    private void geCardCombination() {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 12; j++) {
+                combination[i][j] = rank[j] + suit[i];
+             //   System.out.println(combination[i][j]);
+            }
+        }
+    }
+    private void multiplayer() {
+
+        Random random = new Random();
+        for (int i = 0; i < 2; i++){
+            System.out.println("\nPlayer: "+(i+1));
+            for (int j = 0; j < 9; j++){
+                int randomSuit = random.nextInt(4);
+                int randomRank = random.nextInt(13);
+                player[i][j] = combination[randomSuit][randomRank];
+                System.out.println(player[i][j] + " ");
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        DeckOfCards deckOfCards = new DeckOfCards();
+        System.out.println("Welcome to Deck of Cards game:");
+        deckOfCards.geCardCombination();
+        deckOfCards.multiplayer();
+    }
+}
